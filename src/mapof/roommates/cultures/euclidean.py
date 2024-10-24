bd6fa1e4896ccab5f57dab1b89dbce92b7cfca7a
szufa <s.szufa@gmail.com>
2024-10-24 11:40:28 +0200

    remove old code - cultures

--- src/mapof/roommates/cultures/euclidean.py
+++ src/mapof/roommates/cultures/euclidean.py
@@ -165,17 +165,11 @@ def generate_roommates_mallows_euclidean_votes(num_agents: int = None,
 
     return convert(votes)
 
-# AUXILIARY
-def random_ball(dimension, num_points=1, radius=1):
-    random_directions = np.random.normal(size=(dimension, num_points))
-    random_directions /= linalg.norm(random_directions, axis=0)
-    random_radii = np.random.random(num_points) ** (1 / dimension)
-    return radius * (random_directions * random_radii).T
 
-GEN_CTR = 0
-def get_rand(model: str, i: int = 0, num_agents: int = 0, cat: str = "voters") -> list:
+####################################################
+### UPDATE THIS TO MATCH THE RESAMPLING APPROACH ###
+def get_rand(model: str, i: int = 0, num_agents: int = 0) -> list:
     """ generate random values"""
-    # print(model ==  "1d_uniform")
 
     point = [0]
     if model in {"1d_uniform", "1d_interval"}:
@@ -189,91 +183,62 @@ def get_rand(model: str, i: int = 0, num_agents: int = 0, cat: str = "voters") -
         point = np.random.normal(0.5, 0.15)
         while point > 1 or point < 0:
             point = np.random.normal(0.5, 0.15)
-    elif model == "1d_one_sided_triangle":
-        point = np.random.uniform(0, 1) ** 0.5
-    elif model == "1d_full_triangle":
-        point = np.random.choice(
-            [np.random.uniform(0, 1) ** 0.5, 2 - np.random.uniform(0, 1) ** 0.5])
-    elif model == "1d_two_party":
-        point = np.random.choice([np.random.uniform(0, 1), np.random.uniform(2, 3)])
-    elif model in {"2d_disc", "2d_range_disc"}:
+    # elif model == "1d_one_sided_triangle":
+    #     point = np.random.uniform(0, 1) ** 0.5
+    # elif model == "1d_full_triangle":
+    #     point = np.random.choice(
+    #         [np.random.uniform(0, 1) ** 0.5, 2 - np.random.uniform(0, 1) ** 0.5])
+    # elif model == "1d_two_party":
+    #     point = np.random.choice([np.random.uniform(0, 1), np.random.uniform(2, 3)])
+    elif model in {"2d_disc"}:
         phi = 2.0 * 180.0 * np.random.random()
         radius = math.sqrt(np.random.random()) * 0.5
         point = [0.5 + radius * math.cos(phi), 0.5 + radius * math.sin(phi)]
-    elif model == "2d_range_overlapping":
-        phi = 2.0 * 180.0 * np.random.random()
-        radius = math.sqrt(np.random.random()) * 0.5
-        if cat == "voters":
-            point = [0.25 + radius * math.cos(phi), 0.5 + radius * math.sin(phi)]
-        elif cat == "candidates":
-            point = [0.75 + radius * math.cos(phi), 0.5 + radius * math.sin(phi)]
     elif model in {"2d_square", "2d_uniform"}:
         point = [np.random.random(), np.random.random()]
-    elif model in {'2d_asymmetric'}:
-        if np.random.rand() < 0.3:
-            return np.random.normal(loc=0.25, scale=0.15, size=2)
-        else:
-            return np.random.normal(loc=0.75, scale=0.15, size=2)
-    elif model == "2d_sphere":
-        alpha = 2 * math.pi * np.random.random()
-        x = 1. * math.cos(alpha)
-        y = 1. * math.sin(alpha)
-        point = [x, y]
-    elif model in ["2d_gaussian", "2d_range_gaussian"]:
-        point = [np.random.normal(0.5, 0.15), np.random.normal(0.5, 0.15)]
-        while np.linalg.norm(point - np.array([0.5, 0.5])) > 0.5:
-            point = [np.random.normal(0.5, 0.15), np.random.normal(0.5, 0.15)]
-    elif model in ["2d_range_fourgau"]:
-        r = np.random.randint(1, 4)
-        size = 0.06
-        if r == 1:
-            point = [np.random.normal(0.25, size), np.random.normal(0.5, size)]
-        if r == 2:
-            point = [np.random.normal(0.5, size), np.random.normal(0.75, size)]
-        if r == 3:
-            point = [np.random.normal(0.75, size), np.random.normal(0.5, size)]
-        if r == 4:
-            point = [np.random.normal(0.5, size), np.random.normal(0.25, size)]
-    elif model in ["3d_cube", "3d_uniform"]:
-        point = [np.random.random(), np.random.random(), np.random.random()]
-    elif model in ["5d_uniform"]:
-        dim = 5
-        point = [np.random.random() for _ in range(dim)]
-    elif model in ["10d_uniform"]:
-        dim = 10
-        point = [np.random.random() for _ in range(dim)]
-    elif model in {'3d_asymmetric'}:
-        if np.random.rand() < 0.3:
-            return np.random.normal(loc=0.25, scale=0.15, size=3)
-        else:
-            return np.random.normal(loc=0.75, scale=0.15, size=3)
-    elif model in ['3d_gaussian']:
-        point = [np.random.normal(0.5, 0.15),
-                 np.random.normal(0.5, 0.15),
-                 np.random.normal(0.5, 0.15)]
-        while np.linalg.norm(point - np.array([0.5, 0.5, 0.5])) > 0.5:
-            point = [np.random.normal(0.5, 0.15),
-                     np.random.normal(0.5, 0.15),
-                     np.random.normal(0.5, 0.15)]
-    elif model == "4d_cube":
-        dim = 4
-        point = [np.random.random() for _ in range(dim)]
-    elif model == "5d_cube":
-        dim = 5
-        point = [np.random.random() for _ in range(dim)]
-    elif model == '1d_extreme':
-        if i % 2 == 1:
-            i -= 0.1
-        point = i
-    elif model == '2d_extreme':
-        if i % 2 == 1:
-            alpha = 2 * math.pi * ((i - np.random.random() / 100) / num_agents)
-        else:
-            alpha = 2 * math.pi * ((i + np.random.random() / 100) / num_agents)
-        x = 1. * math.cos(alpha)
-        y = 1. * math.sin(alpha)
-        point = [x, y]
+    # elif model in {'2d_asymmetric'}:
+    #     if np.random.rand() < 0.3:
+    #         return np.random.normal(loc=0.25, scale=0.15, size=2)
+    #     else:
+    #         return np.random.normal(loc=0.75, scale=0.15, size=2)
+    # elif model == "2d_sphere":
+    #     alpha = 2 * math.pi * np.random.random()
+    #     x = 1. * math.cos(alpha)
+    #     y = 1. * math.sin(alpha)
+    #     point = [x, y]
+    # elif model in ["2d_gaussian"]:
+    #     point = [np.random.normal(0.5, 0.15), np.random.normal(0.5, 0.15)]
+    #     while np.linalg.norm(point - np.array([0.5, 0.5])) > 0.5:
+    #         point = [np.random.normal(0.5, 0.15), np.random.normal(0.5, 0.15)]
+    # elif model in ["3d_cube", "3d_uniform"]:
+    #     point = [np.random.random(), np.random.random(), np.random.random()]
+    # elif model in ["5d_uniform"]:
+    #     dim = 5
+    #     point = [np.random.random() for _ in range(dim)]
+    # elif model in ["10d_uniform"]:
+    #     dim = 10
+    #     point = [np.random.random() for _ in range(dim)]
+    # elif model in {'3d_asymmetric'}:
+    #     if np.random.rand() < 0.3:
+    #         return np.random.normal(loc=0.25, scale=0.15, size=3)
+    #     else:
+    #         return np.random.normal(loc=0.75, scale=0.15, size=3)
+    # elif model in ['3d_gaussian']:
+    #     point = [np.random.normal(0.5, 0.15),
+    #              np.random.normal(0.5, 0.15),
+    #              np.random.normal(0.5, 0.15)]
+    #     while np.linalg.norm(point - np.array([0.5, 0.5, 0.5])) > 0.5:
+    #         point = [np.random.normal(0.5, 0.15),
+    #                  np.random.normal(0.5, 0.15),
+    #                  np.random.normal(0.5, 0.15)]
+    # elif model == "4d_cube":
+    #     dim = 4
+    #     point = [np.random.random() for _ in range(dim)]
+    # elif model == "5d_cube":
+    #     dim = 5
+    #     point = [np.random.random() for _ in range(dim)]
     else:
         print('unknown culture_id', model)
         point = [0, 0]
     return point
+
--- src/mapof/roommates/cultures/mallows.py
+++ src/mapof/roommates/cultures/mallows.py
@@ -1,85 +1,11 @@
-import pickle
-
-import numpy as np
-from mapof.roommates.cultures._utils import convert
+import mapof.core.features.mallows as ml
 from mapof.core.utils import *
 
-import mapof.core.features.mallows as ml
+from mapof.roommates.cultures._utils import convert
 
 
 def generate_mallows_votes(*args, **kwargs):
     return ml.generate_mallows_votes(*args, **kwargs)
-
-
-
-def calculateZpoly(m):
-    res = [1]
-    for i in range(1, m + 1):
-        mult = [1] * i
-        res2 = [0] * (len(res) + len(mult) - 1)
-        for o1, i1 in enumerate(res):
-            for o2, i2 in enumerate(mult):
-                res2[o1 + o2] += i1 * i2
-        res = res2
-    return res
-
-
-def evaluatePolynomial(coeff, x):
-    res = 0
-    for i, c in enumerate(coeff):
-        res += c * (x ** i)
-    return res
-
-
-def calculateZ(m, phi):
-    coeff = calculateZpoly(m)
-    return evaluatePolynomial(coeff, phi)
-
-
-# mat[i][j] is the probability with which candidate i ends up in position j
-def mallowsMatrix(num_candidates, lphi, pos, normalize=True):
-    mat = np.zeros([num_candidates, num_candidates])
-    if normalize:
-        phi = ml.phi_from_normphi(num_candidates, lphi)
-    else:
-        phi = lphi
-    Z = calculateZ(num_candidates, phi)
-    for i in range(num_candidates):
-        for j in range(num_candidates):
-            freqs = [pos[k][i][j] for k in
-                     range(1 + int(num_candidates * (num_candidates - 1) / 2))]
-            unnormal_prob = evaluatePolynomial(freqs, phi)
-            mat[i][j] = unnormal_prob / Z
-    return mat
-
-
-def get_mallows_matrix(num_candidates, params, normalize=True):
-    lphi = params['norm-phi']
-    weight = params['weight']
-    # print(lphi, weight)
-    try:
-        path = os.path.join(os.getcwd(), 'mapof', 'voting', 'elections', 'mallows_positionmatrices',
-                            str(num_candidates) + "_matrix.txt")
-        print(path)
-        with open(path, "rb") as file:
-            pos = pickle.load(file)
-    except FileNotFoundError:
-        print("Mallows matrix only supported for up to 30 candidates")
-    # print(pos)
-    mat1 = mallowsMatrix(num_candidates, lphi, pos, normalize)
-    res = np.zeros([num_candidates, num_candidates])
-    for i in range(num_candidates):
-        for j in range(num_candidates):
-            res[i][j] = weight * mat1[i][j] + (1 - weight) * mat1[i][num_candidates - 1 - j]
-    return res
-
-
-
-def runif_in_simplex(n):
-  ''' Return uniformly random vector in the n-simplex '''
-
-  k = np.random.exponential(scale=1.0, size=n)
-  return k / sum(k)
 
 
 def generate_roommates_norm_mallows_votes(num_agents: int = None,
